9b793bf532494f52dd5921cd6f064b31af8812d5
wasif zaman <wasif.zaman1@students.mq.edu.au>
2023-04-22 14:24:18 +1000

    balck jack

--- cards/play.java
+++ cards/play.java
@@ -55,10 +55,13 @@ public class play {
         return "play [hand=" + hand + ", player=" + player + ", score=" + score + ", team=" + team + "]";
     }
 
-    public static void main(String[] args) {
-        
-      TwentyOne();
-    }
+  public static void main(String[] args) {
+
+   
+   
+    TwentyOne();
+     
+  }
 
 
     public static void TwentyOne() {
@@ -135,6 +138,68 @@ public class play {
 
        
     }
+
+
+    public static void TWOplayer() {
+        int CurrentPotPLayer1 = 0;
+        int CurrentPotPLayer2 = 0;
+        int DealerPot = 0;
+        Set<card> hand = new HashSet<>();
+        hand = card.generated(1);
+        Set<card> DealerHand = new HashSet<>();
+           DealerHand = card.generated(1);
+
+           try (Scanner scanner = new Scanner(System.in)) {
+           while (CurrentPot < 21) {
+               System.out.println("player 1");
+               System.out.print("what would you like to do? stand or hit: ");
+               System.out.println("your current hand" + hand.toString());
+               String answer = scanner.nextLine();
+
+               if (answer.equals("stand")) {
+                   System.out.println("you have chosen to stand");
+                   
+                        System.out.println("current total is " + " "+ CurrentPot);
+                           System.out.println("dealer total is " + " "+ DealerPot);
+                           if (CurrentPot > DealerPot && CurrentPot < 21) {
+                               System.out.println("you have won");
+                           } else if (CurrentPot < DealerPot && DealerPot < 21) {
+                               System.out.println("you have lost");
+                           } else {
+                               System.out.println("you have tied");
+                           }
+                           break;
+               } else if (answer.equals("hit")) {
+
+                   System.out.println("you have chosen to hit");
+                   Set<card> hand2 = new HashSet<>();
+                   hand2 = card.generated(1);
+                   Set <card> dealerCards = new HashSet<>();
+                   dealerCards = card.generated(1);
+
+                   for (card card2 : hand2) {
+                       hand.add(card2);
+                   }
+                   System.out.println( "your new card is " + " "+ hand2.toString());
+                   for (card hands: hand) {
+                       CurrentPot += hands.getValue();
+                        
+                    }
+
+                    for (card card : dealerCards) {
+                       DealerHand.add(card);
+                    }
+
+                    for (card card : DealerHand) {
+                       DealerPot += card.getValue();
+                    }
 }
 
+
+           }
+
+        }
+
+    }
+}
 
